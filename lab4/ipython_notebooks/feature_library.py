7dba78f96d06c8dfbeadaa356903c7308c5b9622
user <user@server.fake>
2013-09-26 15:32:54 -0400

    feature library

--- lab4/ipython_notebooks/feature_library.py
+++ lab4/ipython_notebooks/feature_library.py
@@ -8,17 +8,48 @@ import numpy
 # <codecell>
 
 def compute_equal_name( locu_record, foursquare_record ):
-    if locu_record['name'] == foursquare_record['name']:
+    return compute_equal_field( locu_record, foursquare_record, "name" )
+
+def compute_equal_website( locu_record, foursquare_record ):
+    return compute_equal_field( locu_record, foursquare_record, "website" )
+
+def compute_equal_field( locu_record, foursquare_record, field_name ):
+    if locu_record[field_name] == foursquare_record[field_name]:
         return 1.0
     else:
         return 0.0
+    
+
+# <codecell>
+
+
+# <codecell>
+
+def char_splitter(s, n):
+    res = set()
+    if len(s) < n:
+        res.add(s)
+        return res
+
+    for i in range(len(s) - n + 1):
+        res.add(s[i:(i+n)])
+    return res
+
+
+def jaccard_score(p1,p2,fields = ["name", "street_address" ], n = 4):
+    name1 = " ".join([p1[x] for x in fields])
+    name2 = " ".join([p2[x] for x in fields])
+    set1 = set.union(*[char_splitter(x, n) for x in name1.lower().split()])
+    set2 = set.union(*[char_splitter(x, n) for x in name2.lower().split()])
+    c = set1.intersection(set2)
+    return float(len(c)) / (len(set1) + len(set2) - len(c))
 
 # <codecell>
 
 def get_feature_vector( record1, record2 ):
     # return a numpy vector of floats
     feature_vector = []
-    for fn in [ compute_equal_name ]:
+    for fn in [ compute_equal_name, compute_equal_website, jaccard_score ]:
         x = fn( record1, record2 )
     
         feature_vector.append( numpy.float( x ) )
@@ -26,6 +57,10 @@ def get_feature_vector( record1, record2 ):
     return numpy.array( feature_vector )
     
     
+
+    
+    
+
     
 
 # <codecell>
@@ -48,10 +83,13 @@ if __name__ == '__main__':
     
     features = get_feature_vector( r1, r2 )
     
-    for fr in foursquare_records_train[:10]:
-        for lr in locu_records_train[:10]:
+    for fr in foursquare_records_train[:5]:
+        for lr in locu_records_train[:5]:
+            print get_feature_vector( fr, lr )
             
-        
+            #if numpy.sum( get_feature_vector( fr, lr ) ) == 2:
+            #    print fr, lr
+            
     
         
 
