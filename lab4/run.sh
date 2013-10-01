86cb3a95bbb6cf750b69cd2fb993099d733c1ea8
user <user@server.fake>
2013-10-01 09:35:48 -0400

    add run script

--- lab4/run.sh
+++ lab4/run.sh
@@ -1,3 +1,5 @@
+mkdir models
+mkdir working
 python locu_classifier.py models/classifier.pkl
 python locu_matching.py models/classifier.pkl models/matcher.pkl
 python locu_predict.py models/matcher.pkl locu_test_hard.json foursquare_test_hard.json leaderboard_matching.csv
